ebe986b0f1e13be53f1154e40b48a82fd3b21249
Diana Park <dianapark411@gmail.com>
2022-09-23 17:41:51 -0300

    avances 3

--- MyApp/Cuenta.cs
+++ MyApp/Cuenta.cs
@@ -31,7 +31,19 @@ public class Cuenta_corriente_en_pesos: Cuenta{
 
 public class Cuenta_corriente_en_dolares: Cuenta{
     public override void extraccion(double monto, tipo_de_extraccion tipo){
-        
+        if(tipo == tipo_de_extraccion.cajero_automatico){
+            if(monto <= 200){ //hacer bien control del dia xq hasta aca solo contempla el monto de una extraccion
+                Balance_inicial = Balance_inicial - monto;
+                Console.WriteLine("Extraccion exitosa");
+            }else{
+                Console.WriteLine("No se pueden extraer mas de 200 usd por dia en cajero automatico");
+            }                
+        }else{
+            if (tipo == tipo_de_extraccion.cajero_humano){
+                Balance_inicial = Balance_inicial - monto;
+                Console.WriteLine("Extraccion exitosa");
+            }
+        }
     }
 }
 
@@ -43,12 +55,15 @@ public class Caja_ahorro_en_pesos: Cuenta{
             if(tipo == tipo_de_extraccion.cajero_automatico){
                 if(monto <= 10000){
                     Balance_inicial = Balance_inicial - monto;
+                    Console.WriteLine("Extraccion exitosa");
                 }else{
-                    
-                }
-                
+                    Console.WriteLine("No se pueden extraer mas de 10000 por cajero automatico");
+                }                
             }else{
-                if 
+                if (tipo == tipo_de_extraccion.cajero_humano){
+                    Balance_inicial = Balance_inicial - monto;
+                    Console.WriteLine("Extraccion exitosa");
+                }
             }
         }
     }
